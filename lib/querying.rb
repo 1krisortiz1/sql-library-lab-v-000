7a1cdea80fffbf1d38d8e0043313604213a81312
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-12-18 17:17:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -51,7 +51,7 @@ def select_character_names_and_number_of_books_they_are_in
   "
   SELECT Characters.name, COUNT(Books.title)
   FROM Characters
- INNER JOIN Character_books.character_id = Characters.id
+  INNER JOIN Characters_books ON  Characters.id =  Character_books.character_id
   GROUP BY Characters_books.id
   ORDER BY COUNT(Character_books.book_id) DESC"
 end
